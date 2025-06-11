60e71648ead87bc54663aac001ab111f5a223752
fraser <fraser2k19@gmail.com>
2025-06-11 10:45:39 +0100

    added prompts

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+
+
+if len(sys.argv) < 2:
+    print("no promt provided")
+    sys.exit(1)
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=sys.argv[1]
 )
+
 print(response.text)
 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
 print("Response tokens:", response.usage_metadata.candidates_token_count)
+
