ca356c84ed5a78b2588f855c3bd34112f4f6a3c7
fraser <fraser2k19@gmail.com>
2025-06-16 11:13:07 +0100

    get file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -23,3 +23,24 @@ def get_files_info(working_directory, directory=None):
         return "\n".join(files_info)
     except Exception as e:
         return f"Error listing files: {e}"
+    
+def get_file_content(working_directory, file_path):
+    MAX_CHARS = 10000
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = abs_working_dir
+    if file_path:
+        target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+    except Exception as e:
+        return f"Error: failed to read file"
+    if len(file_content_string) == MAX_CHARS:
+        file_content_string += "[...File \"{file_path}\" truncated at 10000 characters]"
+        
+    return file_content_string
+
