139bf1056fc1e260ecb994547fbb26ce4f00f74a
fraser <fraser2k19@gmail.com>
2025-06-17 14:19:56 +0100

    write files

--- tests.py
+++ tests.py
@@ -1,22 +1,16 @@
-from functions.get_files_info import get_file_content
+from functions.get_files_info import write_file
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print("Result for calculator, main:")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for calulator, pkg/calculator.py :")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for calculator, /bin/cat:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
+
 
 if __name__ == "__main__":
     test()
