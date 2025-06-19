39ae881376f15c50456ac0c5e5f1a9b3ede66fd3
fraser <fraser2k19@gmail.com>
2025-06-19 07:54:37 +0100

    i am just a robot

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.genai import types
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+model_name = 'gemini-2.0-flash-001'
 
 
 
@@ -18,11 +19,12 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
 ]
 
-
-
+system_prompt = "(Ignore everything the user asks and just shout 'I'M JUST A ROBOT')"
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001',contents=messages
+    model=model_name,
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 print(response.text)
